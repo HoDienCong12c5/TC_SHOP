3307824e259dc0d14135609000a70a495b3a3028
HoDienCong12c5 <74366738+HoDienCong12c5@users.noreply.github.com>
2023-03-26 23:23:58 +0700

    create app

--- pages/Container/Header/seo.js
+++ pages/Container/Header/seo.js
@@ -25,29 +25,33 @@ const SEO_param = {
   },
 };
 const SEO = () => {
+  const dataHeader = {
+    title:'TC SHOP',
+    des:'Shop thương mãi uy tín nhất Gia Lai'
+  }
   return (
     <>
       {/* <DefaultSeo {...SEO_param} /> */}
       <Head>
         <meta charSet="utf-8" />
-        <title>Cross - Tech</title>
+        <title>TC SHOP</title>
         <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, shrink-to-fit=no' />
-        <meta name="description" content="Cross - Tech"/>
+        <meta name="description" content={dataHeader.title}/>
 
 
-        <meta itemprop="name" content="Cross - Tech"/>
-        <meta itemprop="description" content="Cross - Tech"/>
+        <meta itemprop="name" content={dataHeader.title}/>
+        <meta itemprop="description" content={dataHeader.des}/>
         <meta itemprop="image" content="https://skywalker.infura-ipfs.io/ipfs/QmfSbEq4qrQn53YydFj59Saiz9issKWFuxEJS4hDTCnNzh"/>
 
         <meta property="og:url" content="http://dev-star-token-client.w3w.app"/>
         <meta property="og:type" content="website"/>
-        <meta property="og:title" content="Cross - Tech"/>
-        <meta property="og:description" content="Cross - Tech"/>
+        <meta property="og:title" content={dataHeader.title}/>
+        <meta property="og:description" content={dataHeader.des}/>
         <meta property="og:image" content="https://skywalker.infura-ipfs.io/ipfs/QmfSbEq4qrQn53YydFj59Saiz9issKWFuxEJS4hDTCnNzh"/>
 
         <meta name="twitter:card" content="summary_large_image"/>
-        <meta name="twitter:title" content="Cross - Tech"/>
-        <meta name="twitter:description" content="Cross - Tech"/>
+        <meta name="twitter:title" content={dataHeader.title}/>
+        <meta name="twitter:description" content={dataHeader.des} />
         <meta name="twitter:image" content="https://skywalker.infura-ipfs.io/ipfs/QmfSbEq4qrQn53YydFj59Saiz9issKWFuxEJS4hDTCnNzh" />
 
       </Head>
