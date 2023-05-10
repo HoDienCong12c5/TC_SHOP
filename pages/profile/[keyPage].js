9162576a6a2812ebea2730a7a2698104162c2952
Hồ Diên Thành <74367651+HoDIenThanh12@users.noreply.github.com>
2023-05-10 23:01:12 +0700

    build ui

--- pages/profile/[keyPage].js
+++ pages/profile/[keyPage].js
@@ -10,32 +10,35 @@ import ButtonBasic from '@/Components/ButtonBasic';
 import { NormalText } from '@/Components/TextSize';
 import { Menu } from 'antd';
 import styles from './MintNFT.module.scss'
+import { useState } from 'react';
 const MyProfile = ({keyPage}) => {
-  const route = useRouter()
+  const router = useRouter();
+  const [pageSelected, setPageSelected] = useState(keyPage)
   useEffect(() => {
-    if(Object.values(PAGE_PROFILE).includes(keyPage)){
+    if(!Object.values(PAGE_PROFILE).includes(keyPage)){
+      router.push('/')
     }else{
-      route.push('/')
+      router.push(`/profile/${pageSelected}`)
     }
-  }, []);
-  const pageSelected = useMemo(() => {
-    if(Object.values(PAGE_PROFILE).includes(keyPage)){
-      switch (keyPage) {
-      case PAGE_PROFILE.myProfile:
-        return <UserDetail />
-      default:
-        return <UserDetail />
-      }
-    }else{
-      route.push('/')
+  }, [router,pageSelected]);
+
+  const page = useMemo(() => {
+    switch (pageSelected) {
+    case PAGE_PROFILE.myProfile:
+      return <UserDetail />
+    default:
+      return <></>
     }
-  }, [])
+  }, [pageSelected])
   const renderMenu = () => {
     return <LeftMyProfile>
       <Menu >
         {
           Object.keys(PAGE_PROFILE).map(menu=>{
-            return <ButtonMenu className={styles['btn-menu-main']} key={PAGE_PROFILE[`${menu}`]} >
+            return <ButtonMenu
+              onClick={()=>setPageSelected(PAGE_PROFILE[`${menu}`])}
+              className={styles['btn-menu-main']}
+              key={PAGE_PROFILE[`${menu}`]} >
               <div className='flex gap-10 w-full align-center justify-start  '>
                 <ImageNext className={'w-30 h-30'} size={25} src={ICON_PAGE_PROFILE[`${menu}`]}/>
                 <div className='text-capitalize hover hover__text-underline' >{(PAGE_PROFILE[`${menu}`]).replace('-',' ')}</div>
@@ -53,8 +56,9 @@ const MyProfile = ({keyPage}) => {
       {renderMenu()}
       <RightMyProfile >
         {
-          pageSelected
+          // renderPage()
         }
+        {page}
       </RightMyProfile>
 
     </ContainerMyProfile>
